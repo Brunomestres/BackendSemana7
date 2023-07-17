97920c23593317d899bd2fdda1b241165723e454
Bruno <bruno.mestres@poscontrole.com.br>
2023-07-17 16:49:36 -0300

    cors habilitado

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { routes } from './routes'
+import cors from 'cors'
 import './config/db'
 const PORT = 3333
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/v1', routes)
 
